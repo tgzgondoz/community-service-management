7f397450428fc816f6198548efb76a44e64c871b
tgzgondoz <tgzgondozz@gmail.com>
2026-02-19 13:52:54 +0200

    UI design

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase';
-import { setUserRole } from '../utils/database';
+// Removed unused import: setUserRole
 
 // Hardcoded admin credentials
 const HARDCODED_ADMIN = {
